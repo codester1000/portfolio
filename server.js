c885e267b4abe11d864525d07b9f12df597793ed
Cody McCarney <cody@mccarney.com.au>
2022-09-08 00:13:50 +1000

    wow

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
+ 
 app.listen(port, () => {
    console.log('Server is up!');
 });
